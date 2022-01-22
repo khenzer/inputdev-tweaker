bb7a48755b5c8511842652efc89e3a01628b367f
khenzer <kevin@henzer.ch>
2022-01-22 11:46:27 +0100

    Add main skeleton file. Add meson.build and ignore build directory.

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ * @defgroup   MAIN main
+ *
+ * @brief      Entry point of the inputdev-tweaker daemon
+ *
+ * @author     Kevin
+ * @date       2022
+ */
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	return EXIT_SUCCESS;
+}
